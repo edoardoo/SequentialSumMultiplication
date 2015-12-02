53ccb9d15366c1c8db532660b7a9044ac745c95f
edoardoo <edoubuntu@gmail.com>
2015-12-02 23:23:09 +0000

    Transformed to recursive and added BigInteger support.

--- src/edo/SequentialSum.java
+++ src/edo/SequentialSum.java
@@ -1,21 +1,46 @@
 package edo;
 
+import java.math.BigInteger;
+
 /**
  * Created by edo on 01/12/15.
+ * Repository: https://github.com/edoardoo/SequentialSumMultiplication
+ * Repository contains JUNIT tests.
  *
+ * @author Edoardo Odorico
  *
  */
-public class SequentialSum {
+public class SequentialSum{
 
-    private SequentialSum() {
+    private SequentialSum(){
     }
 
-    public static int multiply( int multiplier, int multiplicand){
-        int result = 0;
+    public static BigInteger multiply(BigInteger multiplier, BigInteger multiplicand){
 
-        for (int i = 0; i < Math.abs( multiplicand ); i++) {
-            result+=multiplier;
+        BigInteger result = recursiveMultiply( multiplier, multiplicand.abs() );
+        return result.multiply( BigInteger.valueOf( multiplicand.signum() ) );
+
+    }
+
+    public static BigInteger multiply( int multiplier, int multiplicand ){
+
+        return multiply( BigInteger.valueOf( multiplier ), BigInteger.valueOf( multiplicand ) );
+
+    }
+
+    private static BigInteger recursiveMultiply( BigInteger multiplier, BigInteger multiplicand ){
+
+        BigInteger result;
+
+        if( multiplicand.equals( BigInteger.ZERO ) ){
+            return BigInteger.ZERO;
+        }else if( multiplicand.equals( BigInteger.ONE ) ){
+            result = multiplier;
+        }else{
+            result = multiplier.add( recursiveMultiply( multiplier, multiplicand.subtract( BigInteger.ONE ) ) );
         }
-        return Integer.signum( multiplicand ) * result;
+        return result;
+
     }
+
 }
--- src/edo/SequentialSumTest.java
+++ src/edo/SequentialSumTest.java
@@ -2,49 +2,72 @@ package edo;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import java.math.BigInteger;
 
 /**
  * Created by edo on 01/12/15.
+ * Repository: https://github.com/edoardoo/SequentialSumMultiplication
+ * Repository contains JUNIT tests.
+ *
+ * @author Edoardo Odorico
+ *
  */
-public class SequentialSumTest {
+public class SequentialSumTest{
 
     @Test
-    public void testMultiplyNormalCondition() throws Exception {
+    public void testMultiplyNormalCondition() throws Exception{
 
-        Assert.assertEquals( 30, SequentialSum.multiply(  10, 3 ) );
+        Assert.assertEquals( BigInteger.valueOf( 30 ), SequentialSum.multiply(  10, 3 ) );
+
     }
 
     @Test
-    public void testMultiplyNegativeMultiplier() throws Exception {
-        Assert.assertEquals( -30, SequentialSum.multiply(  -10, 3 ) );
+    public void testMultiplyNegativeMultiplier() throws Exception{
+
+        Assert.assertEquals( BigInteger.valueOf( -30 ), SequentialSum.multiply(  -10, 3 ) );
+
     }
 
     @Test
-    public void testMultiplyNegativeMultiplicand() throws Exception {
+    public void testMultiplyNegativeMultiplicand() throws Exception{
 
-        Assert.assertEquals( -30, SequentialSum.multiply(  10, -3 ) );
+        Assert.assertEquals( BigInteger.valueOf( -30 ), SequentialSum.multiply(  10, -3 ) );
+
     }
-    @Test
-    public void testMultiplyNegativeMembers() throws Exception {
 
-        Assert.assertEquals( 30, SequentialSum.multiply(  -10, -3 ) );
+    @Test
+    public void testMultiplyNegativeMembers() throws Exception{
+
+        Assert.assertEquals( BigInteger.valueOf( 30 ), SequentialSum.multiply(  -10, -3 ) );
+
     }
-    @Test
-    public void testMultiplyZeroMultiplicand() throws Exception {
 
-        Assert.assertEquals( 0, SequentialSum.multiply(  3, 0 ) );
+    @Test
+    public void testMultiplyZeroMultiplicand() throws Exception{
+
+        Assert.assertEquals( BigInteger.valueOf( 0 ), SequentialSum.multiply(  3, 0 ) );
+
     }
-    @Test
-    public void testMultiplyZeroMultiplier() throws Exception {
 
-        Assert.assertEquals( 0, SequentialSum.multiply(  0, 3 ) );
+    @Test
+    public void testMultiplyZeroMultiplier() throws Exception{
+
+        Assert.assertEquals( BigInteger.valueOf( 0 ), SequentialSum.multiply(  0, 3 ) );
+
     }
-    @Test
-    public void testMultiplyZeroMembers() throws Exception {
 
-        Assert.assertEquals( 0, SequentialSum.multiply(  3, 0 ) );
+    @Test
+    public void testMultiplyZeroMembers() throws Exception{
+
+        Assert.assertEquals( BigInteger.valueOf( 0 ), SequentialSum.multiply(  3, 0 ) );
+
+    }
+
+    @Test
+    public void testMultiplyTooBig() throws Exception{
+
+        Assert.assertEquals( new BigInteger( "6442450941" ), SequentialSum.multiply( Integer.MAX_VALUE, 3 ) );
+
     }
 
 
